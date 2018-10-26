1a9475b7592aa32f250a0ee1405e93830797cb2a
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2018-10-26 11:48:13 -0300

    ocupando rutas hijas con edit, new, detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,25 @@ import {RouterModule, Router} from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+
 
 
 const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'usuario', component: UsuarioComponent},
+  {
+    path: 'usuario/:id', component: UsuarioComponent,
+    //arreglo de rutas hijas
+    children: [
+      {path: 'nuevo', component: UsuarioNuevoComponent},
+      {path: 'editar', component: UsuarioEditarComponent},
+      {path: 'detalle', component: UsuarioDetalleComponent},
+      {path: '**', pathMatch : 'full', redirectTo: 'nuevo'}
+
+    ]
+  },
   {path: '**', pathMatch : 'full', redirectTo: 'home'}
 
 
